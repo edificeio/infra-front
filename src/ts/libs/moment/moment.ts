98c70216c45fadc2bf79a85f7409dd48d2a25aa8
Camille Wintz <camille.wintz@web-education.net>
2016-09-16 11:12:16 +0200

    moment missing on entcore

--- src/ts/libs/moment/moment.ts
+++ src/ts/libs/moment/moment.ts
@@ -2,5 +2,5 @@ export var moment = require('moment');
 if (!window.entcore) {
     window.entcore = {};
 }
-window.entcore.moment = moment.moment;
+window.entcore.moment = moment;
 (window as any).moment = moment;
