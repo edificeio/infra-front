bdfe64f5dbe5493566c1b107747fca9b2541eead
Camille Wintz <camille.wintz@web-education.net>
2016-09-22 18:09:07 +0200

    error when loading jquery

--- src/ts/libs/jquery/jquery.ts
+++ src/ts/libs/jquery/jquery.ts
@@ -1,3 +1,6 @@
-export var $ = require('jquery');
+export let $ = require('jquery');
 (window as any).jQuery = $;
-
+if(!(window as any).entcore){
+    (window as any).entcore = {};
+}
+(window as any).entcore.jQuery = $;
