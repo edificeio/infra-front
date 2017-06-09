06bb392915413571e837d15eb81e2e5138fa994d
Camille Wintz <camille.wintz@web-education.net>
2017-06-09 15:46:27 +0200

    recorder via websockets

--- src/ts/recorder.ts
+++ src/ts/recorder.ts
@@ -1,28 +1,66 @@
+import { moment } from './libs/moment/moment';
 import { idiom as lang } from './idiom';
 import { http } from './http';
 import { notify } from './notify';
+declare const Zlib: any;
 
-var moment = require('moment');
-
-export var recorder = (function(){
+var recorder = (function(){
 	//vendor prefixes
-	(navigator as any).getUserMedia = (navigator as any).getUserMedia || (navigator as any).webkitGetUserMedia ||
+	navigator.getUserMedia = navigator.getUserMedia || (navigator as any).webkitGetUserMedia ||
 		(navigator as any).mozGetUserMedia || (navigator as any).msGetUserMedia;
 	(window as any).AudioContext = (window as any).AudioContext || (window as any).webkitAudioContext;
 
 	var context,
+		ws = null,
+		intervalId,
 		gainNode,
 		recorder,
 		player = new Audio();
 	var leftChannel = [],
 		rightChannel = [];
 
-	var bufferSize = 4096,
+	var bufferSize = 16384,
 		loaded = false,
 		recordingLength = 0,
+		lastIndex = 0,
+		encoder = new Worker('/infra/public/js/audioEncoder.js'),
 		followers = [];
 
-	function notifyFollowers(status, data = undefined){
+	function uuid() {
+		return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+            var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+            return v.toString(16);
+        });
+	}
+
+	function sendWavChunk() {
+		var	index = rightChannel.length;
+		if (!(index > lastIndex)) return;
+		encoder.postMessage(['chunk', leftChannel.slice(lastIndex, index), rightChannel.slice(lastIndex, index), (index - lastIndex) * bufferSize]);
+		encoder.onmessage = function(e) {
+			var deflate = new Zlib.Deflate(e.data);
+			ws.send(deflate.compress());
+		};
+		lastIndex = index;
+	}
+
+	function closeWs() {
+		if (ws) {
+			if (ws.readyState === 1) {
+				ws.close()
+			}
+		}
+        clearWs();
+	}
+
+	function clearWs() {
+		ws = null;
+        leftChannel = [];
+		rightChannel = [];
+		lastIndex = 0;
+	}
+
+	function notifyFollowers(status, data?){
 		followers.forEach(function(follower){
 			if(typeof follower === 'function'){
 				follower(status, data);
@@ -31,13 +69,13 @@ export var recorder = (function(){
 	}
 
 	return {
-        protected: false,
 		elapsedTime: 0,
 		loadComponents: function () {
 		    this.title = lang.translate('recorder.filename') + moment().format('DD/MM/YYYY');
 			loaded = true;
-			(navigator as any).getUserMedia({
-				audio: true
+			
+			navigator.getUserMedia({
+			audio: true
 			}, function(mediaStream){
 				context = new AudioContext();
 				var audioInput = context.createMediaStreamSource(mediaStream);
@@ -46,20 +84,20 @@ export var recorder = (function(){
 
 				recorder = context.createScriptProcessor(bufferSize, 2, 2);
 				recorder.onaudioprocess = function(e){
-					if(this.status !== 'recording' && this.status !== 'paused' && this.status !== 'playing'){
-						mediaStream.stop();
-						loaded = false;
-					}
 					if(this.status !== 'recording'){
 						return;
 					}
-					var left = e.inputBuffer.getChannelData (0);
-					leftChannel.push (new Float32Array(left));
-					var right = e.inputBuffer.getChannelData (1);
-					rightChannel.push (new Float32Array(right));
+					var left = new Float32Array(e.inputBuffer.getChannelData (0));
+					leftChannel.push (left);
+					var right = new Float32Array(e.inputBuffer.getChannelData (1));
+					rightChannel.push (right);
 
 					recordingLength += bufferSize;
+
 					this.elapsedTime += e.inputBuffer.duration;
+
+					sendWavChunk();
+
 					notifyFollowers(this.status);
 				}.bind(this);
 
@@ -69,17 +107,22 @@ export var recorder = (function(){
 			}.bind(this), function(err){
 
 			});
+			
 		},
 		isCompatible: function(){
-			return (navigator as any).getUserMedia !== undefined && (window as any).AudioContext !== undefined;
+			return navigator.getUserMedia !== undefined && (window as any).AudioContext !==undefined;
 		},
 		stop: function(){
+			if (ws) {
+				ws.send("cancel");
+			}
 			this.status = 'idle';
 			player.pause();
 			if(player.currentTime > 0){
 				player.currentTime = 0;
 			}
-
+			leftChannel = [];
+			rightChannel = [];
 			notifyFollowers(this.status);
 		},
 		flush: function(){
@@ -91,14 +134,52 @@ export var recorder = (function(){
 		},
 		record: function(){
 			player.pause();
-			if(player.currentTime > 0){
-				player.currentTime = 0;
-			}
+			var that = this;
+			if (ws) {
+				that.status = 'recording';
+				notifyFollowers(that.status);
+				if(!loaded){
+					that.loadComponents();
+				}
+			} else {
+				ws = new WebSocket((window.location.protocol === "https:" ? "wss": "ws") + "://" +
+						window.location.host + "/audio/" + uuid());
+				ws.onopen = function () {
+					if(player.currentTime > 0){
+						player.currentTime = 0;
+					}
 
-			this.status = 'recording';
-			notifyFollowers(this.status);
-			if(!loaded){
-				this.loadComponents();
+					that.status = 'recording';
+					notifyFollowers(this.status);
+					if(!loaded){
+						http().get('/infra/public/js/zlib.min.js').done(function(){
+							that.loadComponents();
+						}.bind(this));
+					}
+				};
+				ws.onerror = function (event) {
+					console.log(event);
+					that.status = 'stop';
+                    notifyFollowers(that.status);
+                    closeWs();
+                    notify.info(event.data);
+				}
+                ws.onmessage = function (event) {
+                	if (event.data && event.data.indexOf("error") !== -1) {
+                		console.log(event.data);
+						closeWs();
+						notify.info(event.data);
+                	} else if (event.data && event.data === "ok" && recorder.status === 'encoding') {
+                		closeWs();
+                		notify.info("recorder.saved");
+                	}
+
+                }
+                ws.onclose = function (event) {
+                	that.status = 'stop';
+                    notifyFollowers(that.status);
+                    clearWs();
+                }
 			}
 		},
 		pause: function(){
@@ -122,33 +203,12 @@ export var recorder = (function(){
 		},
 		title: "",
 		status: 'idle',
-		save: function(callback, format?){
-			this.stop();
+		save: function(callback, format){
+//			this.stop();
+			sendWavChunk();
+			ws.send("save-" +  this.title);
 			this.status = 'encoding';
 			notifyFollowers(this.status);
-			if(!format){
-				format = 'mp3';
-			}
-
-			var form = new FormData();
-			var encoder = new Worker('/infra/public/js/audioEncoder.js');
-			encoder.postMessage([format, rightChannel, leftChannel, recordingLength]);
-			encoder.onmessage = function(e){
-				this.status = 'uploading';
-				notifyFollowers(this.status);
-				form.append('blob', e.data, this.title + '.' + format);
-				var url = '/workspace/document';
-				if(this.protected){
-					url += '?application=mediaLibrary&protected=true';
-				}
-				http().postFile(url, form).done(function(doc){
-					if(typeof callback === 'function'){
-						callback(doc);
-						this.flush();
-						notify.info('recorder.saved');
-					}
-				}.bind(this));
-			}.bind(this);
 		},
 		mute: function(mute){
 			if(mute){
