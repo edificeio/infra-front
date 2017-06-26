30365164481f2b870e9a6b43e782a858bf99a47f
Camille Wintz <camille.wintz@web-education.net>
2017-06-26 16:56:47 +0200

    export angular on entcore global

--- src/ts/libs/angular/angular.ts
+++ src/ts/libs/angular/angular.ts
@@ -3,3 +3,7 @@ require('angular-route');
 require('angular-sanitize');
 
 export var angular = (window as any).angular;
+if(!(window as any).entcore){
+    (window as any).entcore = {};
+}
+(window as any).entcore.angular = angular;
