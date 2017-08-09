1d37b316d4aa1f39afc9715587258f57ce0ad00b
Camille Wintz <camille.wintz@web-education.net>
2017-08-09 18:04:59 +0200

    clean js

--- src/ts/libs/underscore/underscore.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports._ = require('underscore');
-if (!window.entcore) {
-    window.entcore = {};
-}
-window.entcore._ = exports._;
-//# sourceMappingURL=underscore.js.map
