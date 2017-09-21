312764bbe407f5e51b979b18616f3799971e1586
Camille Wintz <camille.wintz@lweb-education.net>
2017-09-21 15:34:24 +0200

    updated share-panel to match new api

--- src/ts/directives/index.ts
+++ src/ts/directives/index.ts
@@ -18,6 +18,7 @@ export * from './placedBlock';
 export * from './pulsar';
 export * from './recorder';
 export * from './resourceRight';
+export * from './sharePanel';
 export * from './slider';
 export * from './sniplet';
 export * from './sortableList';
