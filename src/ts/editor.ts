00f27bac53cec9d0ac09a442108b5c26543b89f7
Camille Wintz <camille.wintz@lweb-education.net>
2017-11-16 10:34:06 +0100

    hide navbar inside mobile editor

--- src/ts/editor.ts
+++ src/ts/editor.ts
@@ -1195,6 +1195,7 @@ export let RTE = {
                         element.parents('grid-cell').data('lock', false);
                         element.trigger('editor-blur');
                         $('body').css({ overflow: 'auto' });
+                        $('header.main').show();
                     });
 
                     element.find('.editor-toolbar-opener').on('click', function(e){
@@ -1561,6 +1562,7 @@ export let RTE = {
                         element.parents('grid-cell').data('lock', true);
                         if ($(window).width() < ui.breakpoints.tablette) {
                             $('body').css({ overflow: 'hidden' });
+                            $('header.main').hide();
                             window.scrollTo(0,0);
                             setTimeout(function(){
                                 var sel = document.getSelection();
