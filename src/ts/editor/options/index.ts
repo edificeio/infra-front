3aadc67207ebde414aa0b4c54fff063bfeb70eda
Camille Wintz <camille.wintz@lweb-education.net>
2017-11-24 13:44:19 +0100

    move editor options for smaller screens

--- src/ts/editor/options/index.ts
+++ src/ts/editor/options/index.ts
@@ -3,9 +3,10 @@ export * from './redo';
 export * from './bold';
 export * from './italic';
 export * from './underline';
-export * from './justify';
 export * from './font';
 export * from './format';
+export * from './justify';
+
 export * from './attachment';
 export * from './image';
 export * from './templates';
