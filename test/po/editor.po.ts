6bbddb0731dbfde45b589032e17b290c841955ce
Simon Basset <simon.basset@gmail.com>
2018-11-05 13:26:26 +0100

    fix(editor): issue 19611, selection fixes on delete and empty editor
    - move onPressDelete to a separated file
    - remove a selection change when pressing delete in an unempty line
    - adds a selection change when pressing enter in an empty editor
    - little refactor + fix typo in pressDelete tests

--- test/po/editor.po.ts
+++ test/po/editor.po.ts
@@ -1,11 +1,6 @@
 export default new class EditorPo {
     private selector = `.drawing-zone`;
 
-    navigateToPage(): void {
-        browser.url('/editor');
-        this.editor.waitForVisible();
-    }
-
     waitForVisible(): WebdriverIO.Client<boolean> {
         return browser.waitForVisible(this.selector);
     }
@@ -13,6 +8,4 @@ export default new class EditorPo {
     get editor(): WebdriverIO.Client<WebdriverIO.Element> {
         return browser.element<WebdriverIO.Element>(this.selector);
     }
-
-
 }
