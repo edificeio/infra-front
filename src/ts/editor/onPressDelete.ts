7f06e6fc8e8705859517f9f7da81fe227dacff26
Simon Basset <simon.basset@gmail.com>
2018-11-05 18:07:05 +0100

    fix(editor): fix line deletion
    - fix the way we delete the current line when pressing delete in an
    empty line
    - refactor test helpers of onPressDelete and onPressEnter
    - check if a node is a HTMLElement before checking if it is a block
    element

--- src/ts/editor/onPressDelete.ts
+++ src/ts/editor/onPressDelete.ts
@@ -1,4 +1,13 @@
 import { $ } from "../libs";
+import { findClosestHTMLElement, findLatestChildTextNode, isHTMLBlockElement } from "./selection";
+
+function findClosestBlockElement(currentNode: Node, root: Node): Node {
+    let target;
+    while ((currentNode = findClosestHTMLElement(currentNode)) && isHTMLBlockElement(currentNode) && currentNode !== root && !target) {
+        target = currentNode;
+    }
+    return target;
+}
 
 export function onPressDelete(event, selection, editorInstance, editZone) {
     editorInstance.addState(editZone.html());
@@ -7,30 +16,47 @@ export function onPressDelete(event, selection, editorInstance, editZone) {
     for (var i = 0; i < selection.rangeCount; i++) {
         var r = selection.getRangeAt(i);
         var startContainer = r.startContainer;
-        var selfContent = startContainer.textContent.replace(/[\u200B-\u200D\uFEFF]/g, ''); // Delete empty white spaces
         if (startContainer.nodeType === 1 && startContainer.nodeName === 'TD' || startContainer.nodeName === 'TR') {
             (startContainer as any).remove();
-        }
-        // Also delete the empty div for return after backslash
-        else if (startContainer.nodeName === '#text' && selfContent.length === 0) {
-            if (startContainer.parentNode.previousSibling) {
-                var node = startContainer.parentNode.previousSibling.lastChild;
-                // Needs to place the caret correctly for firefox
-                setTimeout(function () {
-                    var range = document.createRange();
-                    range.setStart(node, node.textContent.length);
-                    selection.removeAllRanges();
-                    selection.addRange(range);
-                }, 1);
-                event.preventDefault();
-                (startContainer.parentNode as any).remove();
+        } else {
+            if (startContainer !== editZone.get(0)) {
+                let blockElement: Node;
+                if (isHTMLBlockElement(startContainer)) {
+                    blockElement = startContainer;
+                } else {
+                    blockElement = findClosestBlockElement(startContainer, editZone.get(0));
+                }
+                if (blockElement && blockElement.previousSibling) {
+                    if (blockElement.textContent.replace(/[\u200B-\u200D\uFEFF]/g, '') === '') {
+                        const previous = blockElement.previousSibling;
+                        if (previous.nodeType === Node.TEXT_NODE) {
+                            const range = document.createRange();
+                            range.setStart(previous, previous.textContent.length);
+                            selection.removeAllRanges();
+                            selection.addRange(range);
+                        } else {
+                            let textNode = findLatestChildTextNode(previous);
+                            if (!textNode) {
+                                textNode = document.createTextNode('');
+                                previous.appendChild(textNode);
+                            }
+                            const range = document.createRange();
+                            range.setStart(textNode, textNode.textContent.length);
+                            selection.removeAllRanges();
+                            selection.addRange(range);
+                        }
+                        event.preventDefault();
+                        blockElement.parentNode.removeChild(blockElement);
+                    }
+                }
+
             }
-        }
-        // If the current line already contain text
-        else if (r.startOffset === 1 && startContainer.parentNode.previousSibling) {
-            var previousContent = startContainer.parentNode.previousSibling.lastChild.textContent.replace(/[\u200B-\u200D\uFEFF]/g, '');
-            if (startContainer.parentNode.previousSibling.lastChild.nodeName === '#text' && previousContent.length === 0) {
-                (startContainer.parentNode.previousSibling as any).remove();
+            // If the current line already contain text
+            else if (r.startOffset === 1 && startContainer.parentNode.previousSibling) {
+                var previousContent = startContainer.parentNode.previousSibling.lastChild.textContent.replace(/[\u200B-\u200D\uFEFF]/g, '');
+                if (startContainer.parentNode.previousSibling.lastChild.nodeName === '#text' && previousContent.length === 0) {
+                    startContainer.parentNode.previousSibling.parentNode.removeChild(startContainer.parentNode.previousSibling);
+                }
             }
         }
     }
