90dd836602a2c1248bd84768edb3d4817ec5d896
Simon Basset <simon.basset@gmail.com>
2018-11-07 10:05:20 +0100

    fix(editor): fix previous line deletion when pressing delete at the beginning of a line

--- src/ts/editor/onPressDelete.ts
+++ src/ts/editor/onPressDelete.ts
@@ -11,30 +11,47 @@ export function findClosestBlockElement(currentNode: Node, root: Node): Node {
     return target;
 }
 
+function trim(text: string): string {
+    return text.replace(/[\u200B-\u200D\uFEFF]/g, '');
+}
+
+function trimBeforeIndex(text: string, index: number): string {
+    return trim(text.substring(0, index)) + text.substring(index);
+}
+
+function trimNodeBeforeRange(node: Node, range: Range): { node: Node, range: Range } {
+    const startContainer = range.startContainer;
+    const startOffset = range.startOffset;
+    const endContainer = range.endContainer;
+    const endOffset = range.endOffset;
+
+    const initialTextLength = startContainer.textContent.length;
+
+    startContainer.textContent = trimBeforeIndex(startContainer.textContent, startOffset);
+
+    const textLengthDiff = initialTextLength - startContainer.textContent.length;
+
+    range.setStart(startContainer, startOffset - textLengthDiff);
+    if (endContainer === startContainer) {
+        range.setEnd(endContainer, endOffset - textLengthDiff);
+    }
+
+    return {node, range};
+}
+
 export function onPressDelete(event, selection, editorInstance, editZone) {
     editorInstance.addState(editZone.html());
-    // for whatever reason, ff likes to create several ranges for table selection
-    // which messes up their deletion
-    for (var i = 0; i < selection.rangeCount; i++) {
-        var r = selection.getRangeAt(i);
-        var startContainer = r.startContainer;
 
-        if (r.startContainer.nodeType === Node.TEXT_NODE) {
-            let oldStartOffset = r.startOffset;
-            let oldEndContainer = r.endContainer;
-            let oldEndOffset = r.endOffset;
-            let oldLength = startContainer.textContent.length;
-            startContainer.textContent =
-                startContainer.textContent.substring(0, oldStartOffset).replace(/[\u200B-\u200D\uFEFF]/g, '')
-                + startContainer.textContent.substring(oldStartOffset);
-            r.setStart(startContainer, oldStartOffset - (oldLength - startContainer.textContent.length));
-            if (oldEndContainer === startContainer) {
-                r.setEnd(oldEndContainer, oldEndOffset - (oldLength - oldEndContainer.textContent.length))
-            }
+    for (let i = 0; i < selection.rangeCount; i++) {
+        const range = selection.getRangeAt(i);
+        const startContainer = range.startContainer;
+
+        if (startContainer.nodeType === Node.TEXT_NODE) {
+            trimNodeBeforeRange(startContainer, range);
         }
 
-        if (startContainer.nodeType === 1 && startContainer.nodeName === 'TD' || startContainer.nodeName === 'TR') {
-            (startContainer as any).remove();
+        if (startContainer.nodeType === Node.ELEMENT_NODE && startContainer.nodeName === 'TD' || startContainer.nodeName === 'TR') {
+            startContainer.parentNode.removeChild(startContainer);
         } else {
             if (startContainer !== editZone.get(0)) {
                 let blockElement: Node;
@@ -44,18 +61,18 @@ export function onPressDelete(event, selection, editorInstance, editZone) {
                     blockElement = findClosestBlockElement(startContainer, editZone.get(0));
                 }
                 if (blockElement && blockElement.previousSibling) {
-                    if (blockElement.textContent.replace(/[\u200B-\u200D\uFEFF]/g, '') === '') {
-                        const previous = blockElement.previousSibling;
-                        if (previous.nodeType === Node.TEXT_NODE) {
+                    const previousElement = blockElement.previousSibling;
+                    if (trim(blockElement.textContent).length === 0) { // current line can be deleted
+                        if (previousElement.nodeType === Node.TEXT_NODE) {
                             const range = document.createRange();
-                            range.setStart(previous, previous.textContent.length);
+                            range.setStart(previousElement, previousElement.textContent.length);
                             selection.removeAllRanges();
                             selection.addRange(range);
                         } else {
-                            let textNode = findLatestChildTextNode(previous);
+                            let textNode = findLatestChildTextNode(previousElement);
                             if (!textNode) {
                                 textNode = document.createTextNode('');
-                                previous.appendChild(textNode);
+                                previousElement.appendChild(textNode);
                             }
                             const range = document.createRange();
                             range.setStart(textNode, textNode.textContent.length);
@@ -64,16 +81,15 @@ export function onPressDelete(event, selection, editorInstance, editZone) {
                         }
                         event.preventDefault();
                         blockElement.parentNode.removeChild(blockElement);
+                    } else if (trim(previousElement.textContent).length === 0 &&
+                        range.startOffset === 0 &&
+                        (range.startContainer === range.endContainer) &&
+                        (range.startOffset === range.endOffset)) { // previousElement line can be deleted
+                        previousElement.parentNode.removeChild(previousElement);
+                        event.preventDefault();
                     }
                 }
 
-            }
-            // If the current line already contain text
-            else if (r.startOffset === 1 && startContainer.parentNode.previousSibling) {
-                var previousContent = startContainer.parentNode.previousSibling.lastChild.textContent.replace(/[\u200B-\u200D\uFEFF]/g, '');
-                if (startContainer.parentNode.previousSibling.lastChild.nodeName === '#text' && previousContent.length === 0) {
-                    startContainer.parentNode.previousSibling.parentNode.removeChild(startContainer.parentNode.previousSibling);
-                }
             }
         }
     }
