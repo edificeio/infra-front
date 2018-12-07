98dcd02e6ee7e16480bedcf9afd3b8072fe67ae5
Simon Basset <simon.basset@gmail.com>
2018-12-07 16:29:23 +0100

    fix(editor): issue 22471, fix apply style to a block node

--- test/po/editor.po.ts
+++ test/po/editor.po.ts
@@ -1,11 +1,24 @@
+export class ToolbarPo {
+    private justifyRightSelector = `.option.justify-right`;
+
+    get justifyRight(): WebdriverIO.Client<WebdriverIO.Element> {
+        return browser.element<WebdriverIO.Element>(this.justifyRightSelector);
+    }
+}
+
 export default new class EditorPo {
-    private selector = `.drawing-zone`;
+    private contentSelector = `.drawing-zone`;
+    private toolbarPo = new ToolbarPo();
 
     waitForVisible(): WebdriverIO.Client<boolean> {
-        return browser.waitForVisible(this.selector);
+        return browser.waitForVisible(this.contentSelector);
     }
 
-    get editor(): WebdriverIO.Client<WebdriverIO.Element> {
-        return browser.element<WebdriverIO.Element>(this.selector);
+    get content(): WebdriverIO.Client<WebdriverIO.Element> {
+        return browser.element<WebdriverIO.Element>(this.contentSelector);
+    }
+
+    get toolbar(): ToolbarPo {
+        return this.toolbarPo;
     }
 }
