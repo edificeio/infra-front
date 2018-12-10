338713f754f24ff31e6b58b649e2454d63be295b
Simon Basset <simon.basset@gmail.com>
2018-12-10 14:21:19 +0100

    fix(editor): issue 22535, whitelist possible tags for new line
    - also run end-to-end tests locally

---
+++ test/specs/editor.format.spec.ts
@@ -0,0 +1,17 @@
+import { openNewBlogPostPage, setWideScreen } from './spec-helper';
+import editor from '../po/editor.po';
+
+describe('editor press enter in a formatted line', () => {
+    it('should add a new line with a <div></div> when pressing enter', () => {
+        setWideScreen();
+        openNewBlogPostPage();
+        editor.content.click();
+        editor.content.addValue('Enter');
+        editor.content.addValue('abc');
+        editor.toolbar.format.click();
+        editor.toolbar.format.element('h2').click();
+        editor.content.addValue('Enter');
+        expect(editor.content.getHTML(false))
+            .toBe('<div>\u200b</div><h2>\u200babc</h2><div>\u200b</div>');
+    });
+});
