bdcb63f3fb671e1f384b4594e580723b2cd2cff4
Simon Basset <simon.basset@gmail.com>
2018-12-12 10:26:45 +0100

    fix(editor): issue 22472, fix selection issue with safari

---
+++ test/specs/editor.press-delete.spec.ts
@@ -0,0 +1,16 @@
+import editor from '../po/editor.po';
+import { openNewBlogPostPage, setWideScreen } from './spec-helper';
+
+describe('editor press delete', () => {
+    it('should remove the character before the caret', () => {
+        setWideScreen();
+        openNewBlogPostPage();
+        editor.content.click();
+        editor.content.addValue('Enter');
+        editor.content.addValue('Enter');
+        editor.content.addValue('abc');
+        editor.content.addValue('Backspace');
+        expect(editor.content.getHTML(false))
+            .toBe('<div>\u200b</div><div>\u200b</div><div>ab</div>');
+    });
+});
