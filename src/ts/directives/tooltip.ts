54ce8b53ca4f6d0ba42cc4e2ca46f11deaad153e
ccou <celiac26@live.fr>
2018-12-19 17:43:57 +0100

    add source element and refacto

--- src/ts/directives/tooltip.ts
+++ src/ts/directives/tooltip.ts
@@ -4,137 +4,133 @@ import { $ } from '../libs/jquery/jquery';
 import { template } from '../template';
 import { ui } from '../ui';
 
-function tooltipLink ($compile, scope, element, attributes, tooltipShoudBeDisplayed){
-        let tgtElement = element;
-        if (attributes.tooltipTargetSelector) {
-            tgtElement = element.find(attributes.tooltipTargetSelector);
-            if (!tgtElement) return;
+function tooltipLink($compile: any, scope: any, element: any, attributes: {[name: string]: string}, tooltipDisplayCondition: (any) => boolean, tooltipType: string, sourceElement: any) {
+    let targetElement = element;
+    if (attributes.tooltipTargetSelector) {
+        targetElement = element.find(attributes.tooltipTargetSelector);
+        if (!targetElement) return;
+    }
+
+    let restrictToElement;
+    if (attributes.tooltipRestrictSelector) {
+        restrictToElement = targetElement.parents(attributes.tooltipRestrictSelector);
+        if (restrictToElement.length !== 1) {
+            restrictToElement = undefined;
         }
+    }
 
-        let restrictToElement;
-        if (attributes.tooltipRestrictSelector) {
-            restrictToElement = tgtElement.parents(attributes.tooltipRestrictSelector);
-            if (restrictToElement.length !== 1) restrictToElement = undefined;
-        }
+    let tooltipCheck = true;
+    //you can add a condition to display the tooltip
+    if (element[0].hasAttribute('tooltip-check')) {
+        scope.$watch(() => scope.$eval(attributes.tooltipCheck), (newVal) => {
+            tooltipCheck = newVal ? true : false;
+        });
+    }
+    // Handle multiline
+    if (element[0].hasAttribute('tooltip-check-content')) {
+        scope.$watch(() => { return element[0].offsetHeight }, () => {
+            tooltipCheck = element[0].offsetHeight < element[0].scrollHeight;
+        });
+    }
 
-        var tooltipCheck = true;
-        //you can add a condition to display the tooltip
-        if(element[0].hasAttribute('tooltip-check')){
-            scope.$watch(() => scope.$eval(attributes.tooltipCheck), (newVal) => {
-                if(!newVal){
-                    tooltipCheck = false;
-                }
-                else{
-                    tooltipCheck = true
-                }
-            });
-        }
-        // Handle multiline
-        if(element[0].hasAttribute('tooltip-check-content')) {
-            scope.$watch(function() { return element[0].offsetHeight }, function (newValue, oldValue) {
-                tooltipCheck = element[0].offsetHeight < element[0].scrollHeight;
-            });
-        }
+    let tip;
+    targetElement.on('mouseenter', function () {
 
-        var tip;
-        tgtElement.on('mouseenter', function() {
+        if (attributes.tooltipTemplate) {
+            let templatePath = template.getPath(attributes.tooltipTemplate);
+            tip = $('<div />')
+                .addClass('tooltip')
+                .html($compile('<div ng-include=\'\"' + templatePath + '\"\'></div>')(scope))
+                .appendTo('body');
+            scope.$apply();
 
-            if (attributes.tooltipTemplate) {
-                var tplPath = template.getPath(attributes.tooltipTemplate);
-                tip = $('<div />')
-                    .addClass('tooltip')
-                    .html($compile('<div ng-include=\'\"' + tplPath + '\"\'></div>')(scope))
-                    .appendTo('body');
-                scope.$apply();
+            tip.css('position', 'absolute');
+            let top = targetElement.offset().top;
+            let left = parseInt(targetElement.offset().left - tip.width() - 5);
 
-                tip.css('position', 'absolute');
-                let top  = tgtElement.offset().top;
-                let left = parseInt(tgtElement.offset().left - tip.width() - 5);
-
-                if (restrictToElement) {
-                    if (left < restrictToElement.offset().left) {
-                        left = parseInt(tgtElement.offset().left + tgtElement.width() + 5);
-                    }
-
-                    if (left + tgtElement.width() > restrictToElement.offset().left + restrictToElement.width()) {
-                        left = restrictToElement.offset().left + 5;
-                        top += 30;
-                    }
-
-                    if (top > restrictToElement.height() + 150) {
-                        top -= 130;
-                    }
+            if (restrictToElement) {
+                if (left < restrictToElement.offset().left) {
+                    left = parseInt(targetElement.offset().left + targetElement.width() + 5);
                 }
 
-                tip.css('left', left);
-                tip.css('top', top);
-            } else {
-                if($(window).width() < ui.breakpoints.tablette || !attributes.tooltip || !tooltipCheck || attributes.tooltip === 'undefined'){
-                    return;
+                if (left + targetElement.width() > restrictToElement.offset().left + restrictToElement.width()) {
+                    left = restrictToElement.offset().left + 5;
+                    top += 30;
                 }
-                tip = $('<div />')
-                    .addClass('tooltip')
-                    .html($compile('<div class="arrow"></div><div class="content">' + idiom.translate(attributes.tooltip) + '</div> ')(scope))
-                    .appendTo('body');
-                scope.$apply();
 
-                var top = parseInt(element.offset().top + element.outerHeight());
-                var left = parseInt(element.offset().left + element.width() / 2 - tip.width() / 2);
-                if(top < 5){
-                    top = 5;
-                }
-                if(left < 5){
-                    left = 5;
-                }
-                tip.offset({
-                    top: top,
-                    left: left
-                });
-                if(tooltipShoudBeDisplayed(element)) {
-                    tip.fadeIn();
-                    element.one('mouseout', function(){
-                        tip.fadeOut(200, function(){
-                            $(this).remove();
-                        })
-                    });
+                if (top > restrictToElement.height() + 150) {
+                    top -= 130;
                 }
             }
 
-            if(tooltipShoudBeDisplayed(element)) {
+            tip.css('left', left);
+            tip.css('top', top);
+        } else {
+            if ($(window).width() < ui.breakpoints.tablette || !attributes[tooltipType] || !tooltipCheck || attributes[tooltipType] === 'undefined') {
+                return;
+            }
+            tip = $('<div />')
+                .addClass('tooltip')
+                .html($compile('<div class="arrow"></div><div class="content">' + idiom.translate(attributes[tooltipType]) + '</div> ')(scope))
+                .appendTo('body');
+            scope.$apply();
+
+            let top = parseInt(sourceElement.offset().top + sourceElement.outerHeight());
+            let left = parseInt(sourceElement.offset().left + sourceElement.width() / 2 - tip.width() / 2);
+            if (top < 5) {
+                top = 5;
+            }
+            if (left < 5) {
+                left = 5;
+            }
+            tip.offset({
+                top: top,
+                left: left
+            });
+            if (tooltipDisplayCondition(element)) {
                 tip.fadeIn();
-                tgtElement.one('mouseleave', function () {
+                element.one('mouseout', function () {
                     tip.fadeOut(200, function () {
                         $(this).remove();
                     })
                 });
             }
-        });
+        }
 
-        scope.$on("$destroy", function() {
-            if(tip){
-                tip.remove();
-            }
+        if (tooltipDisplayCondition(element)) {
+            tip.fadeIn();
+            targetElement.one('mouseleave', function () {
+                tip.fadeOut(200, function () {
+                    $(this).remove();
+                })
+            });
+        }
+    });
 
-            tgtElement.off();
-        });
+    scope.$on("$destroy", function () {
+        if (tip) {
+            tip.remove();
+        }
 
-    }
+        targetElement.off();
+    });
+
+}
 
 
-export const tooltip = ng.directive('tooltip', ['$compile', function($compile){
+export const tooltip = ng.directive('tooltip', ['$compile', function ($compile) {
     return {
         restrict: 'A',
-        link: function(scope, element, attributes) {
-            tooltipLink($compile, scope, element, attributes, () => true)
+        link: function (scope, element, attributes) {
+            tooltipLink($compile, scope, element, attributes, () => true, "tooltip", element)
         }
     }
 }]);
-export const tooltipEllipzed = ng.directive('tooltipe', ['$compile', function($compile){
+export const tooltipOnEllipsis = ng.directive('tooltipOnEllipsis', ['$compile', function ($compile) {
     return {
         restrict: 'A',
-        link: function(scope, element, attributes) {
-
-            tooltipLink($compile, scope, element, attributes, e => (e.parent('a').offsetWidth < e.parent('a').scrollWidth))
+        link: function (scope, element, attributes) {
+            tooltipLink($compile, scope, element, attributes, element => (element.parent('a').get(0).offsetWidth < element.parent('a').get(0).scrollWidth),"tooltipOnEllipsis", element.parent('a'))
         }
-    }
+    } 
 }]);
