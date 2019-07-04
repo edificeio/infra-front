9f960168bb54895f1aad3a80cbc7b91477f58301
Simon Basset <simon.basset@gmail.com>
2019-07-04 15:57:13 +0200

    fix: #21776, adds a new attribute on infotip to specify the display when infotip is open

--- src/ts/directives/infotip.ts
+++ src/ts/directives/infotip.ts
@@ -1,6 +1,5 @@
 import { ng } from '../ng-start';
 import { Me } from '../me';
-import http from 'axios';
 
 export let infotip = ng.directive('infotip', () => {
     return {
@@ -9,20 +8,20 @@ export let infotip = ng.directive('infotip', () => {
             <i class="close"></i>
             <div ng-transclude></div>
         `,
-        scope: { name: '@', onChange: '&' },
+        scope: {name: '@', onChange: '&'},
         transclude: true,
+
         link: async (scope, element, attributes) => {
             const onChange = function () {
                 let isFalse = infotips[scope.name] === false;
-                scope.onChange && scope.onChange({ '$visible': !isFalse })
-            }
+                scope.onChange && scope.onChange({'$visible': !isFalse});
+            };
             let infotips = await Me.preference('infotip');
             onChange();
             if (infotips[scope.name] === false) {
                 element.remove();
-            }
-            else {
-                element.css({ 'display': 'block' });
+            } else {
+                element.css({'display': !!attributes.display ? attributes.display : 'block'});
             }
 
             element.children('i').on('click', () => {
@@ -33,4 +32,4 @@ export let infotip = ng.directive('infotip', () => {
             })
         }
     }
-})
+});
