fc20e3df1ddaf8ead534be8643e8cdd7077ca377
Valentin Mourot <valentin.mourot@opendigitaleducation.com>
2019-09-04 18:02:43 +0200

    feat(workflow): #26329 add `workflow-not` directive as the inverse of `workflow` directive

--- src/ts/directives/workflow.ts
+++ src/ts/directives/workflow.ts
@@ -2,39 +2,63 @@ import { ng } from '../ng-start';
 import { model } from '../modelDefinitions';
 import { Behaviours } from '../behaviours';
 
-export let workflow = ng.directive('workflow', ['$compile', function($compile){
-	return {
-		restrict: 'A',
-		link: function(scope, element, attributes){
-			attributes.$observe('workflow', async () => {
-				const auth = attributes.workflow.split('.');
-                if(!Behaviours.applicationsBehaviours[auth[0]]){
-                    throw "Behaviours from application " + auth[0] + " missing";
-                }
-                if(Behaviours.applicationsBehaviours[auth[0]].callbacks){
-                    await Behaviours.load(auth[0]);
-                }
+const workflowDirectiveLinkBuilder = (name:string) =>
+	(func: (right: boolean, content: any, element: any) => void) =>
+		function (scope, element, attributes) {
+			attributes.$observe(name, async () => {
+				const auth = attributes[name].split('.');
+				if (!Behaviours.applicationsBehaviours[auth[0]]) {
+					throw "Behaviours from application " + auth[0] + " missing";
+				}
+				if (Behaviours.applicationsBehaviours[auth[0]].callbacks) {
+					await Behaviours.load(auth[0]);
+				}
 				let right = model.me && model.me.workflow;
 				const workflow = !!(right && right[auth[0]]);
 				const content = element.children();
-				if(!right || !workflow){
-					content.remove();
-					element.hide();
+				if (!right || !workflow) {
+					func(false, content, element);
 					return;
 				}
-				auth.forEach(function(prop){
+				auth.forEach(function (prop) {
 					right = right[prop];
 				});
-				
-				if(!right){
+				func(right, content, element);
+			});
+		};
+
+export let workflow = ng.directive('workflow', ['$compile', function($compile){
+	return {
+		restrict: 'A',
+		link: workflowDirectiveLinkBuilder('workflow')(
+			(right, content, element) => {
+				if (!right) {
 					content.remove();
 					element.hide();
 				}
-				else{
+				else {
 					element.show();
 					element.append(content);
 				}
-			});
-		}
+			}
+		)
+	}
+}]);
+
+export let workflowNot = ng.directive('workflowNot', ['$compile', function ($compile) {
+	return {
+		restrict: 'A',
+		link: workflowDirectiveLinkBuilder('workflowNot')(
+			(right, content, element) => {
+				if (right) {
+					content.remove();
+					element.hide();
+				}
+				else {
+					element.show();
+					element.append(content);
+				}
+			}
+		)
 	}
 }]);
