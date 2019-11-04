1534ffdcfc75ce5671fa935334ef12a300b680ff
Nabil Mansouri <nabil.mansouri.3@gmail.com>
2019-11-04 10:36:28 +0100

    feat(navigationGuard): Add navigation guard service

--- src/ts/directives/navigationGuard.ts
+++ src/ts/directives/navigationGuard.ts
@@ -1,8 +1,36 @@
 import { Directive, ng } from "../ng-start";
 
 
+export const initGuardDirective: Directive = ng.directive('init-guard', () => {
+    return {
+        restrict: 'A',
+        link: function(scope, element, attrs, ngModel) {
+        }
+    }
+});
 
-export const textGuardDirective: Directive = ng.directive('text-guard', () => {
+
+
+export const resetGuardDirective: Directive = ng.directive('reset-guard', () => {
+    return {
+        require: 'ngModel',
+        restrict: 'A',
+        link: function(scope, element, attrs, ngModel) {
+        }
+    }
+});
+
+export const inputGuardDirective: Directive = ng.directive('text-guard', () => {
+    return {
+        require: 'ngModel',
+        restrict: 'A',
+        link: function(scope, element, attrs, ngModel) {
+            //viewChangeListeners
+        }
+    }
+});
+
+export const imageGuardDirective: Directive = ng.directive('image-guard', () => {
     return {
         require: 'ngModel',
         restrict: 'A',
