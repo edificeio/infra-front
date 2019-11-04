e69396f25267ea3b7ab5079ca8ef7017bfbdf270
Valentin PEIRO <valentin.peiro@opendigitaleducation.com>
2019-11-04 17:36:58 +0100

    feat(navigatonGuard): Update directives

--- src/ts/directives/navigationGuard.ts
+++ src/ts/directives/navigationGuard.ts
@@ -1,59 +1,59 @@
 import { Directive, ng } from "../ng-start";
-import { InputGuard, navigationGuardService } from "../navigationGuard";
+import { INavigationGuard, InputGuard, navigationGuardService, AngularJSRouteChangeListener, DOMRouteChangeListener } from "../navigationGuard";
 import { Element } from "../workspace/model";
 
 
-export const initGuardDirective: Directive = ng.directive('initGuard', () => {
+export const guardRootDirective: Directive = ng.directive('guardRoot', () => {
     return {
         restrict: 'A',
         link: function (scope, element, attrs, ngModel) {
+            navigationGuardService.registerListener(new AngularJSRouteChangeListener(scope.$root));
+            navigationGuardService.registerListener(new DOMRouteChangeListener());
+
         }
     }
 });
 
+function generateGuardDirective(directiveName: string, guardFactory: (scope, element, attrs, ngModel) => INavigationGuard): Directive
+{
+    return ng.directive(directiveName, () =>
+    {
+        return {
+            require: 'ngModel',
+            restrict: 'A',
+            link: function(scope, element, attrs, ngModel)
+            {
+                const guard = guardFactory(scope, element, attrs, ngModel);
 
+                if(guard != null)
+                {
+                    navigationGuardService.registerGuard(guard);
+                    scope.$on("$destroy", function()
+                    {
+                        navigationGuardService.unregisterGuard(guard);
+                    });   
+                }
+            },
+        };
+    });
+};
 
-export const resetGuardDirective: Directive = ng.directive('resetGuard', () => {
-    return {
-        require: 'ngModel',
-        restrict: 'A',
-        link: function (scope, element, attrs, ngModel) {
-        }
-    }
+export const resetGuardDirective: Directive = generateGuardDirective('resetGuard', () => {
+    return null;
 });
 
-export const inputGuardDirective: Directive = ng.directive('inputGuard', () => {
-    return {
-        require: 'ngModel',
-        restrict: 'A',
-        link: function (scope, element, attrs, ngModel) {
-            const guard = new InputGuard<any>(() => ngModel.$viewValue, () => ngModel.$viewValue);
-            navigationGuardService.registerGuard(guard);
-            scope.$on("$destroy", function () {
-                navigationGuardService.unregisterGuard(guard);
-            });
-        }
-    }
+export const inputGuardDirective: Directive = generateGuardDirective('inputGuard', (scope, element, attrs, ngModel) => {
+    return new InputGuard<any>(() => ngModel.$viewValue, () => ngModel.$viewValue);
 });
 
-export const documentGuardDirective: Directive = ng.directive('documentGuard', () => {
-    return {
-        require: 'ngModel',
-        restrict: 'A',
-        link: function (scope, element, attrs, ngModel) {
-            const guard = new InputGuard<Element>(() => ngModel.$modelValue && ngModel.$modelValue.id,
-                () => ngModel.$modelValue ? ngModel.$modelValue.id : undefined,
-                (a, b) => {
-                    if (a && b) {
-                        return a._id == b._id;
-                    } else {
-                        return !a && !b;
-                    }
-                });
-            navigationGuardService.registerGuard(guard);
-            scope.$on("$destroy", function () {
-                navigationGuardService.unregisterGuard(guard);
-            });
-        }
-    }
+export const documentGuardDirective: Directive = generateGuardDirective('documentGuard', (scope, element, attrs, ngModel) => {
+    return new InputGuard<Element>(() => ngModel.$modelValue && ngModel.$modelValue.id,
+        () => ngModel.$modelValue ? ngModel.$modelValue.id : undefined,
+        (a, b) => {
+            if (a && b) {
+                return a._id == b._id;
+            } else {
+                return !a && !b;
+            }
+        });
 });
