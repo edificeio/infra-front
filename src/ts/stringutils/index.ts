3d99e95f2c3da62c20d898ffe43819239cd7e0d3
Valentin PEIRO <valentin.peiro@opendigitaleducation.com>
2019-11-13 12:15:25 +0100

    fix(exercizer): #19453, Try to fix the StrUtils export

--- src/ts/stringutils/index.ts
+++ src/ts/stringutils/index.ts
@@ -4,3 +4,8 @@ export class StrUtils
 {
   static removeDiacritics: (str: string) => string = removeDiacritics;
 };
+
+if(!(window as any).entcore){
+	(window as any).entcore = {};
+}
+(window as any).entcore.StrUtils = StrUtils;
