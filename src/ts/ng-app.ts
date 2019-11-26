92b433ebc7df643dae5eb031b9652eed5f637de7
richard.bunel@opendigitaleducation.com <richard.bunel@opendigitaleducation.com>
2019-11-26 21:25:54 +0100

    feat: #28472, created simple enter key event directive

--- src/ts/ng-app.ts
+++ src/ts/ng-app.ts
@@ -2409,3 +2409,15 @@ model.one("bootstrap", async () => {
 		injectIntoSelector("body", "<cgu-lightbox></cgu-lightbox>");
 	}
 })
+module.directive('onEnter', function () {
+    return function (scope, element, attrs) {
+        element.bind("keydown keypress", function (event) {
+            if(event.which === 13) {
+                scope.$apply(function (){
+                    scope.$eval(attrs.onEnter);
+                });
+                event.preventDefault();
+            }
+        });
+    };
+});
