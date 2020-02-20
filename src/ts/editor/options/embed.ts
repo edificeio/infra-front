543e674a92ea06da3dba0d52d154b7bd83702a28
Nabil Mansouri <nabil.mansouri.3@gmail.com>
2020-02-20 11:23:57 +0100

    feat(video): #30874, Remove cookie and manage start using events

--- src/ts/editor/options/embed.ts
+++ src/ts/editor/options/embed.ts
@@ -3,7 +3,7 @@ export const embed = {
     run: function (instance) {
         return {
             template: '<i ng-click="display.copyEmbed = true" tooltip="editor.option.embed"></i>' +
-            '<embedder file-format="\'video\'" hidden-share-video-code=' + instance.hiddenShareVideoCode + ' ng-model="display.htmlCode" ng-change="applyHtml()" show="display.copyEmbed"></embedder>',
+            '<embedder file-format="\'video\'" hidden-share-video-code=' + instance.hiddenShareVideoCode + ' ng-model="display.htmlCode" ng-change="applyHtml()" ng-if="display.copyEmbed" show="display.copyEmbed"></embedder>',
             link: function (scope, element, attributes) {
                 scope.display = {
                     htmlCode: ''
