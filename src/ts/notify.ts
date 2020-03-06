5c14a958aa6df7ccb1ea60ee08b0f69f850041c0
Nabil Mansouri <nabil.mansouri.3@gmail.com>
2020-03-06 09:31:02 +0100

    fix(pdf): #31170, Dont display notification on print mode

--- src/ts/notify.ts
+++ src/ts/notify.ts
@@ -23,3 +23,17 @@ if(!(window as any).entcore){
 }
 (window as any).entcore.notify = notify;
 (window as any).notify = notify;
+
+//pupetter mode => no notification
+if((window as any).pupetterMode){
+	notify.message= (type, message)=>{
+		switch(type){
+			case 'error':
+				console.error(message);
+				break;
+			default:
+				console.log(message);
+				break;
+		}
+	}
+}
