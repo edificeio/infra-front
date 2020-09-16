d83414de8788f5f3deae253aad90aa0ca986b635
sjol <samuel.jollois@cgi.com>
2020-09-16 12:14:32 +0200

    Remove audio/video feature from editor when requested

--- src/ts/editor/options/embed.ts
+++ src/ts/editor/options/embed.ts
@@ -1,18 +1,25 @@
 export const embed = {
     name: 'embed',
     run: function (instance) {
-        return {
-            template: '<i ng-click="display.copyEmbed = true" tooltip="editor.option.embed"></i>' +
-            '<embedder file-format="\'video\'" hidden-share-video-code=' + instance.hiddenShareVideoCode + ' ng-model="display.htmlCode" ng-change="applyHtml()" ng-if="display.copyEmbed" show="display.copyEmbed"></embedder>',
-            link: function (scope, element, attributes) {
-                scope.display = {
-                    htmlCode: ''
-                };
-
-                scope.applyHtml = function (template) {
-                    instance.selection.replaceHTML(scope.display.htmlCode);
-                    scope.display.copyEmbed=false;
-                };
+        if(!instance.hiddenShareVideoCode) {
+            return {
+                template: '<i ng-click="display.copyEmbed = true" tooltip="editor.option.embed"></i>' +
+                    '<embedder file-format="\'video\'" hidden-share-video-code=' + instance.hiddenShareVideoCode + ' ng-model="display.htmlCode" ng-change="applyHtml()" ng-if="display.copyEmbed" show="display.copyEmbed"></embedder>',
+                link: function (scope, element, attributes) {
+                    scope.display = {
+                        htmlCode: ''
+                    };
+                    scope.applyHtml = function (template) {
+                        instance.selection.replaceHTML(scope.display.htmlCode);
+                        scope.display.copyEmbed=false;
+                    };
+                }
+            }
+        } else {
+            return {
+                template: '',
+                link: function (scope, element, attributes) {
+                }
             }
         }
     }
