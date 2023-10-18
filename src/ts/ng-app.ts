6f476d01a4fe72ed9db7df3baa6c182818338be1
Romu-C <romuald.coulaud@gmail.com>
2023-10-18 13:32:36 +0200

    revert fix: #WB-2026 (moved to branch fix-WB-2026)

--- src/ts/ng-app.ts
+++ src/ts/ng-app.ts
@@ -755,25 +755,21 @@ module.directive("portal", [
       restrict: "E",
       transclude: true,
       templateUrl: function (element, attributes) {
-        const version =
-          (window as any).springboardBuildDate || new Date().getTime();
+
+        const version = (window as any).springboardBuildDate || new Date().getTime()
         return attributes.templateUrl
-          ? attributes.templateUrl + "?version=" + version
-          : skin.portalTemplate + "?version=" + version;
+          ? attributes.templateUrl+"?version="+version
+          : skin.portalTemplate+"?version="+version;
       },
       compile: function (element, attributes, transclude) {
         // Initialize any configured tracker
         tracker.init();
 
-        if (
-          model.me.hasWorkflow(
-            "fr.openent.chatbot.controller.ChatbotController|view"
-          )
-        ) {
-          Behaviours.loadBehaviours("chatbot", function () {
-            Behaviours.applicationsBehaviours.chatbot.chatbot.init();
-          });
-        }
+		if (model.me.hasWorkflow('fr.openent.chatbot.controller.ChatbotController|view')) {
+			Behaviours.loadBehaviours('chatbot', function () {
+				Behaviours.applicationsBehaviours.chatbot.chatbot.init();
+			});
+		}
 
         $("html").addClass("portal-container");
         element
@@ -2905,7 +2901,7 @@ module.controller("Account", [
           ) {
             $scope.avatar = skin.basePath + "/img/illustrations/no-avatar.svg";
           }
-          $scope.username = model.me.firstName;
+          $scope.username = result.result["0"].displayName;
           model.me.profiles = result.result["0"].type;
           $scope.$apply();
         });
