add95a63982d3f787f74c0a5b30006817bcfc000
Headan <lucien.devarsdumayne@opendigitaleducation.com>
2024-02-07 13:49:09 +0100

    fix: #WB2-902, show banner public blog hdf + save close with cookies

--- src/ts/directives/infotip.ts
+++ src/ts/directives/infotip.ts
@@ -1,56 +1,47 @@
-import { ng } from '../ng-start';
-import { Me } from '../me';
+import { ng } from "../ng-start";
 
-export let infotip = ng.directive('infotip', () => {
-    return {
-        restrict: 'E',
-        template: `
+export let infotip = ng.directive("infotip", () => {
+  return {
+    restrict: "E",
+    template: `
             <i class="close"></i>
             <div ng-transclude></div>
         `,
-        scope: {name: '@', onChange: '&', savePreferenceUnder: "@", showOnce: "="},
-        transclude: true,
+    scope: {
+      name: "@",
+      onChange: "&",
+      savePreferenceUnder: "@",
+      showOnce: "=",
+    },
+    transclude: true,
 
-        link: async (scope, element, attributes) => {
-            // 2 cases, depending on the savePreferenceUnder scoped value.
-            let key = 'infotip';
-            if( angular.isString(scope.savePreferenceUnder) && scope.savePreferenceUnder.trim().length > 0 ) {
-                key = scope.savePreferenceUnder.trim();
-            }
+    link: async (scope, element, attributes) => {
+      let key = "infotip";
+      if (
+        angular.isString(scope.savePreferenceUnder) &&
+        scope.savePreferenceUnder.trim().length > 0
+      ) {
+        key = scope.savePreferenceUnder.trim();
+      }
 
-            try {
-                await Me.preference(key);
-            } catch (error) {
-                console.error(error)
-            }
+      element.css({'display': 'block'});
 
-            // Helper get/set function
-            var visibility = function(value?: Boolean) {
-                if( arguments.length <= 0 ) return Me.preferences[key][scope.name]!==false ? true : false;
-                else                        Me.preferences[key][scope.name] = value;
-            }
+      scope.$$showPublicInfoTipRgpdCookie = false;
 
-            const notifyVisibility = function () {
-                scope.onChange && scope.onChange( {'$visible': visibility()} );
-            };
+      var showInfoTip = window.localStorage.getItem("showInfoTip.cookies");
+      if (showInfoTip != "false") {
+        scope.$$showPublicInfoTipRgpdCookie = true;
+      }
 
-            notifyVisibility();
-            if( !visibility() ) {
-                element.remove();
-            } else {
-                element.css({'display': 'block'});
-                if( scope.showOnce ) {
-                    visibility( false ); // Do not notify this visibility change.
-                    Me.savePreference( key );
-                }
-            }
+      var closeBanner = () => {
+        scope.$$showPublicInfoTipRgpdCookie = false;
+        window.localStorage.setItem("showInfoTip.cookies", "false");
+      };
 
-            element.children('i').on('click', () => {
-                element.slideUp();
-                visibility( false );
-                notifyVisibility();
-                Me.savePreference( key );
-            })
-        }
-    }
+      element.children("i").on("click", () => {
+        element.slideUp();
+        closeBanner();
+      });
+    },
+  };
 });
